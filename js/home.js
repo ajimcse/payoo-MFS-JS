2fa16302180aa537baddcbd57c9e6b420179e6a7
ajimcse <mdajimsorkarcse40@gmail.com>
2026-07-13 23:25:16 +0600

    balance add

---
+++ js/home.js
@@ -0,0 +1,21 @@
+document.getElementById('add-money-btn').addEventListener('click', function (event) {
+    event.preventDefault()
+    const addAmountInput = document.getElementById('add-amount-input').value;
+
+    const yourPassword = document.getElementById('add-password-input').value;
+
+    if (yourPassword === '1') {
+        const availableBalance = document.getElementById('available-balance').innerText;
+        const addMoneyInputNUmber = parseFloat(addAmountInput);
+        const balanceNumber = parseFloat(availableBalance)
+        const totalCurrenBalance = addMoneyInputNUmber + balanceNumber
+
+        document.getElementById('available-balance').innerText = totalCurrenBalance
+
+    }
+ 
+    else {
+        alert('this is woring password')
+    }
+
+})
