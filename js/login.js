35d54ebf4db1ce50f47b1a80e6ae5703800c3f91
ajimcse <mdajimsorkarcse40@gmail.com>
2026-07-13 20:43:13 +0600

    login button

---
+++ js/login.js
@@ -0,0 +1,13 @@
+document.getElementById('btn-login').addEventListener('click', function(event){
+    event.preventDefault()
+
+    const yourNumber = document.getElementById('your-number').value;
+    const yourPassword = document.getElementById('your-password').value;
+    if(yourNumber ==='1' && yourPassword === 'ajim123'){
+        console.log('WellCome to Login');
+        window.location.href='./home.html'
+    }
+    else{
+        alert('woring your number and Password')
+    }
+})
